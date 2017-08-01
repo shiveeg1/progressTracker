47482e4d43e7b52b4c4f3d8dfdacc6971211ed74
shiveeg1 <shiveegpt77@gmail.com>
2017-08-02 00:32:15 +0530

    intial setup

--- progressTracker/progressTracker/settings.py
+++ progressTracker/progressTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n9+5*w_r@fm9a=#p4tzh3ubgiez(k+u1rmljllq%2^^o!7)2_h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
