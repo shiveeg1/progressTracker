5b379769f258459ba8516bb3c54baa27b1cfe635
shiveeg1 <shiveegpt77@gmail.com>
2017-08-10 01:59:05 +0530

    landing page setup, todo static path detection from settings file

--- progressTracker/mason/views.py
+++ progressTracker/mason/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.template import RequestContext
 # Create your views here.
+def landingPage(request):
+    return render(request,'index.html',{'STATIC_URL':'../static/'})
--- progressTracker/progressTracker/settings.py
+++ progressTracker/progressTracker/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'progressTracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +66,8 @@ TEMPLATES = [
         },
     },
 ]
+
+#TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
 
 WSGI_APPLICATION = 'progressTracker.wsgi.application'
 
@@ -117,6 +118,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
