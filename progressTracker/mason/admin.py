912268d85f0dbec16be7e9f77c6333e25a4fb789
shiveeg1 <shiveegpt77@gmail.com>
2017-08-09 22:46:47 +0530

    admin settings
    removing .idea/* files

--- progressTracker/mason/admin.py
+++ progressTracker/mason/admin.py
@@ -1,3 +1,15 @@
+from mason.models import *
 from django.contrib import admin
 
-# Register your models here.
+# view the models on http://localhost:8000/admin/
+admin.site.register(role)
+admin.site.register(userMetadata)
+admin.site.register(brick)
+admin.site.register(brickType)
+admin.site.register(pillar)
+admin.site.register(constructionProject)
+admin.site.register(workSprint)
+admin.site.register(skillLevel)
+admin.site.register(userProject)
+admin.site.register(group)
+admin.site.register(userGroupDetails)
