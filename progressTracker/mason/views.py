034599cffbd7814d82c0a7e7fa684668851265d3
shiveeg1 <shiveegpt77@gmail.com>
2017-08-11 01:43:38 +0530

    views for signup

--- progressTracker/mason/views.py
+++ progressTracker/mason/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate
+from django.http import HttpResponseRedirect, HttpResponse
+from models import *
+
 from django.template import RequestContext
-# Create your views here.
+
+
 def landingPage(request):
     return render(request,'index.html',{'STATIC_URL':'../static/'})
 
@@ -25,3 +29,34 @@ def authentication(request):
     except Exception as e:
         print(e);
         return HttpResponse("Invalid input given. Cannot authenticate user !!!");
+
+
+def signupVerification(username, email):
+    # if user already exits return 1
+    user = User.objects.get(username=username, email=email)
+    if User.objects.filter(username=username, email=email).exists():
+        return 1
+    # if username is taken return 2
+    if User.objects.filter(username=username).exists():
+        return 2
+
+
+def signup(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/homepage/')
+    if request.method == "POST":
+        username = request.get('username')
+        email = request.get('email')
+        if signupVerification(username,email) == 1:
+           return HttpResponse(request,'signUp.html',{'verificationMsg' :'The user already exists. Please login.' })
+        elif signupVerification(username,email) == 2:
+            return HttpResponse(request,'signUp.html',{'verificationMsg' :'The username is unavailable. Please try another name.' })
+        if request.method == "POST":
+            username = request.get('username')
+            password = request.get('password')
+            firstName = request.get('firstName')
+            lastName  = request.get('lastName')
+            User.objects.create_user(username,email=email,password=password, first_name=firstName, last_name=lastName)
+            return HttpResponse(0)
+    return HttpResponse(1)
+
