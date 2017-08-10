62bb6e61d28921d7cfc88ea31b7dd1c5e19759c6
vivek.tiwari <vivek.tiwari@mu-sigma.com>
2017-08-10 23:40:01 +0530

    create new branch with single screen change

--- progressTracker/mason/urls.py
+++ progressTracker/mason/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^home$', views.landingPage, name='landingPage'),
+    url(r'^$',views.index, name='indexRoute'),
+    url(r'^authenticate',views.authentication, name='authentication'),
+    url(r'.*',views.notFound,name='No Match'),
 ]
