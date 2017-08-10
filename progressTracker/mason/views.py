200ca73fb77f2cfc2af0119840b993cc5dc5f656
shiveeg1 <shiveegpt77@gmail.com>
2017-08-11 02:28:34 +0530

    signup fixes

--- progressTracker/mason/views.py
+++ progressTracker/mason/views.py
@@ -32,7 +32,7 @@ def authentication(request):
 
 def signupVerification(username, email):
     # if user already exits return 1
-    user = User.objects.get(username=username, email=email)
+    #user = User.objects.get(username=username, email=email)
     if User.objects.filter(username=username, email=email).exists():
         return 1
     # if username is taken return 2
@@ -45,17 +45,17 @@ def createUser(request):
     #     return HttpResponse("You have signed up!")
     print (request)
     if request.method == "POST":
-        username = request.get('username')
-        email = request.get('email')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
         if signupVerification(username,email) == 1:
            return HttpResponse(request,'signUp.html',{'verificationMsg' :'The user already exists. Please login.' })
         elif signupVerification(username,email) == 2:
             return HttpResponse(request,'signUp.html',{'verificationMsg' :'The username is unavailable. Please try another name.' })
         if request.method == "POST":
-            username = request.get('username')
-            password = request.get('password')
-            firstName = request.get('firstName')
-            lastName  = request.get('lastName')
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            firstName = request.POST.get('firstName')
+            lastName  = request.POST.get('lastName')
             User.objects.create_user(username,email=email,password=password, first_name=firstName, last_name=lastName)
             return HttpResponse(0)
     return HttpResponse(1)
